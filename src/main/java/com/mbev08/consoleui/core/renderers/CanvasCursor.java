f174beabf44d34cca3ae55d702138304d694e336
mbev08 <mbeverly2021@gmail.com>
2025-07-07 23:57:22 -0500

    Added skip controls to CanvasCursor.

--- src/main/java/com/mbev08/consoleui/core/renderers/CanvasCursor.java
+++ src/main/java/com/mbev08/consoleui/core/renderers/CanvasCursor.java
@@ -21,4 +21,23 @@ public class CanvasCursor {
         this.row = 0;
     }
 
+    /**
+     * Skips <i>n</i> columns starting at current position.
+     *
+     * @param n     =   number of columns to skip.
+     */
+    public void skipColumns(int n) {
+        this.column += n;
+    }
+
+    /**
+     * Skips <i>n</i> rows starting at current position.
+     *
+     * @param n     =   number of rows to skip.
+     */
+    public void skipRows(int n) {
+        this.row += n;
+    }
+
+
 }
